c3f50176718cbfef0e7d44c4a47979cc6671814c
Alessandra Braga <alessandraaraujo.2105@gmail.com>
2025-07-31 17:20:53 +0000

    Modifica a frase e adiciona o audio

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
+// pages/index.js
+
 function Home() {
-    return <h1>Píncipe lindo, eu amo você. Se você me ama, dá uma risadinha! 🥰❤️</h1>
+  return (
+    <div> {/* Adicionei uma div para envolver tudo */}
+      <h1>Píncipe & Pincesa ❤️🥰</h1>
+      <p>Píncipe lindo, eu amo você. Se você me ama, dá uma risadinha! 🤭❤️</p>
+
+      <audio controls>
+        <source src="/audio/musica_para_pincipe.mp3" type="audio/mpeg" />
+        Seu navegador não suporta o elemento de áudio.
+      </audio>
+
+    </div>
+  );
 }
 
 export default Home;
- 
